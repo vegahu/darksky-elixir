4f7b415e3af317a7d84272e962601b6e77153e05
Hugo Vega <hugotcc@gmail.com>
2016-10-02 21:46:01 +0200

    forecast & timemachine functions done

--- lib/darksky.ex
+++ lib/darksky.ex
@@ -1,2 +1,29 @@
 defmodule Darksky do
+
+@moduledoc """
+This module warps the two requests of Darksky API:
+  - Forescast
+  - Timemachine
+
+The result is a map.
+
+A key is needed.
+
+Go to <https://darksky.net/dev/> for a key and documentation.
+
+
+"""
+
+
+  def forecast(latitude, longitude, api_key, opts \\ %{}) when is_map(opts) do
+    forecast = HTTPotion.get("https://api.darksky.net/forecast/#{api_key}/#{latitude},#{longitude}", [query: opts])
+    JSON.decode(forecast.body)
+  end
+
+  def timemachine(latitude, longitude, date, api_key, opts \\ %{}) when is_number(date) and is_map(opts) do
+    forecast = HTTPotion.get("https://api.darksky.net/forecast/#{api_key}/#{latitude},#{longitude},#{date}", [query: opts])
+    JSON.decode(forecast.body)
+
+  end
+
 end
