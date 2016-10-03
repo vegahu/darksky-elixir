2b3f8d04df3170bed79d925bf784c9ac26659d87
Hugo Vega <hugotcc@gmail.com>
2016-10-03 18:17:15 +0200

    Update darksky.ex

--- lib/darksky.ex
+++ lib/darksky.ex
@@ -10,8 +10,6 @@ The result is a map.
 A key is needed.
 
 Go to <https://darksky.net/dev/> for a key and documentation.
-
-
 """
 
 @doc """
@@ -28,7 +26,6 @@ Returns the observed or forecast weather conditions for a date in the past or fu
   def timemachine(api_key, latitude, longitude, date, opts \\ %{}) when is_number(date) and is_map(opts) do
     forecast = HTTPotion.get("https://api.darksky.net/forecast/#{api_key}/#{latitude},#{longitude},#{date}", [query: opts])
     JSON.decode(forecast.body)
-
   end
 
 end
