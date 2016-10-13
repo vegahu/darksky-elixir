485afb4fef2cfadfeee4b162d4059ee955e66def
Hugo Vega <hugotcc@gmail.com>
2016-10-13 19:07:37 +0200

    tests

--- test/darksky_test.exs
+++ test/darksky_test.exs
@@ -2,7 +2,20 @@ defmodule DarkskyTest do
   use ExUnit.Case
   doctest Darksky
 
-  test "the truth" do
-    assert 1 + 1 == 2
+
+  test "forecast without options" do
+    assert {:ok, _} = Darksky.forecast("YOUR-API-KEY", 43.545, -5.6626)
+  end
+
+  test "forecast with options" do
+    assert {:ok, _} = Darksky.forecast("YOUR-API-KEY", 43.545, -5.6626, %{exclude: "currently,minutely,hourly,daily,alerts,flags", extend: "hourly", lang: "es", units: "auto"})
+  end
+
+  test "timemachine without options" do
+    assert {:ok, _} = Darksky.timemachine("YOUR-API-KEY", 43.545, -5.6626, "2016-10-01T12:12:12")
+  end
+
+  test "timemachine with options" do
+    assert {:ok, _} = Darksky.timemachine("YOUR-API-KEY", 43.545, -5.6626, "2016-10-01T12:12:12", %{exclude: "currently,minutely,hourly,daily,alerts,flags", extend: "hourly", lang: "es", units: "auto"})
   end
 end
